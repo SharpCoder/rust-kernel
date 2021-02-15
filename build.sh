1d31d0f54b96380982b28d1ce2d8d2504f2ee085
Josh Cole <colemangrill@live.com>
2021-02-15 07:58:31 -0800

    initial blinky

---
+++ build.sh
@@ -0,0 +1,12 @@
+rm -rf out || true
+mkdir out || true
+
+# Rust compilation
+rustc --target arm-unknown-linux-gnueabihf -o out/kernel.o -O --emit=obj src/kernel.rs
+arm-none-eabi-ld -T src/boot/linker.ld out/kernel.o -o out/kernel.elf
+arm-none-eabi-objdump -D out/kernel.o > out/kernel.list
+arm-none-eabi-nm out/kernel.o > out/kernel.headers
+
+# Final image generation
+arm-none-eabi-objcopy out/kernel.elf -O binary out/boot.bin
+cat src/boot/toc.bin src/boot/header.bin out/boot.bin > out/rom.img
---
+++ src/kernel.rs
@@ -0,0 +1,57 @@
+#![feature(lang_items, asm)]
+#![crate_type = "staticlib"]
+#![no_std]
+
+const GPIO_BASE: u32 = 0x4804C000;
+const GPIO_OE_OFFSET: u32 = 0x134;
+const GPIO_DATAOUT: u32 = 0x13C;
+const CM_BASE: u32 = 0x44E00000;
+const CM_CLKCTRL_OFFSET: u32 = 0xAC;
+
+#[no_mangle]
+pub fn main() {
+    loop {
+        init();
+        set_led(true);
+        sleep(10000000);
+        set_led(false);
+        sleep(10000000);
+    }
+}
+
+fn sleep(cycles: u32) {
+    for _ in 0..cycles {
+        unsafe { asm!("nop"); }
+    }
+}
+
+fn set_led(on: bool) {
+    let gpio_ptr = (GPIO_BASE + GPIO_DATAOUT) as *mut u32;
+    if on {
+        unsafe { *gpio_ptr = 0xFFFFFFFF; }
+    } else {
+        unsafe { *gpio_ptr = 0x0; }
+    }
+}
+
+fn init() {
+    let clock_ptr = (CM_BASE + CM_CLKCTRL_OFFSET) as *mut u32;
+    let gpio_oe_ptr = (GPIO_BASE + GPIO_OE_OFFSET) as *mut u32;
+    unsafe {
+        *clock_ptr = 0x2;
+        *gpio_oe_ptr = 0x00;
+    }
+}
+
+#[lang = "eh_personality"]
+#[no_mangle]
+pub extern fn eh_personality() {}
+
+#[panic_handler]
+#[no_mangle]
+pub extern fn my_panic(_info: &core::panic::PanicInfo) -> ! {
+    loop {}
+}
+
+#[no_mangle]
+pub extern "C" fn __aeabi_unwind_cpp_pr0() { }
